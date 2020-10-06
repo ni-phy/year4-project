a31b037a8e4b3b4b161be0470e106bb3d70301ca
ni-phy <55056340+ni-phy@users.noreply.github.com>
2020-10-06 11:31:36 +0300

    improved model and new training/sample data

--- Sklearn_test.py
+++ Sklearn_test.py
@@ -11,34 +11,34 @@ def sinusoid(x):
 def generate_1d_data(x, num_training_points, observation_noise_variance):
 
   observations_ = (sinusoid(x) +
-                   np.random.normal(loc=0,
-                                    scale=np.sqrt(observation_noise_variance),
-                                    size=(num_training_points)))
+                   np.random.normal(loc=x,
+                                    scale=np.sqrt(observation_noise_variance)))#, size=(num_training_points)))
   return observations_
 
 # Generate training data with a known noise level (we'll later try to recover
 # this value from the data).
 
 num = 100
-x = np.atleast_2d(np.linspace(0, 2, num=num)).T
+x = np.atleast_2d(np.linspace(0, 1.5, num=num)).T
+x_more = np.atleast_2d(np.linspace(2.2, 3, num=num)).T
+x = np.concatenate((x, x_more))
 observations_ = generate_1d_data(x, num, 0.1)
-kernel = 2 * RBF(4.0)
-gpf = gp.GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10)
+kernel = 2*RBF(1.0)
+gpf = gp.GaussianProcessRegressor(kernel=None, n_restarts_optimizer=30)
 
 gpf.fit(x, observations_)
-x_re = np.atleast_2d(np.linspace(2,4, num=num)).T
+x_re = np.atleast_2d(np.linspace(1,2.5, num=num)).T
 pred, std = gpf.predict(x_re, return_std=True)
-#plt.figure()
-x_plot = np.linspace(0, 4)
-plt.plot(x_re, sinusoid(x_re), 'r:', label=r'$f(x) = x\,\sin(x)$')
-plt.plot(x, observations_, 'r.', label='Observations')
+print(x.shape, observations_.shape)
+y = generate_1d_data(x_re, num, 0.1)
+plt.scatter(x_re, y, c='grey', label=r'$f(x) = x\,\sin(x)$ + Noise')
+plt.scatter(x, observations_, c='r', label='Observations')
 plt.plot(x_re, pred, 'b-', label='Prediction')
 plt.fill(np.concatenate([x_re, x_re[::-1]]),
          np.concatenate([pred - 1.9600 * std,
                         (pred + 1.9600 * std)[::-1]]),
-         alpha=.5, fc='grey', ec='None', label='95% confidence interval')
+         alpha=.5, fc='grey', ec='None')#, label='95% confidence interval')
 plt.xlabel('$x$')
 plt.ylabel('$f(x)$')
-#plt.legend(loc='upper left')
+plt.legend(loc='upper left')
 plt.show()
-#Std is too large
